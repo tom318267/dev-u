948e9c1fe6f417bf094c7a27e19d20f7251a5b7b
tom318267 <ogar318267@gmail.com>
2022-01-20 14:55:30 -0500

    add border radius to tutor card

--- components/TutorCard.js
+++ components/TutorCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const TutorCard = ({ image, name }) => {
   return (
     <div className="flex flex-col justify-center items-center mb-12 lg:col-span-2">
-      <img src={image} alt="" />
+      <img className="rounded-md" src={image} alt="" />
       <h2 className="font-mont font-semibold text-[20px] mt-4">{name}</h2>
       <p className="font-mohave text-[16px] w-[234px] text-center">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare sit ut
